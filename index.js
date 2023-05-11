e7f84f47b345b06a67645d8b4ac7306b074b173c
achyutjagini <achyut.jagini@gmail.com>
2023-05-11 14:42:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,5 +40,5 @@ app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server started on ${port}`)
+  console.log(`server started on ${PORT}`)
 })
